10bce62c15f333faae324ed590e688cfb61f88f4
ltrudeau <ltrudeau@mozilla.com>
2016-12-01 12:30:15 -0500

    Reduce memory consumption and added DCT Domain subsampling

--- tf/aom_tf.c
+++ tf/aom_tf.c
@@ -8,10 +8,17 @@
 
 #include "vidinput.h"
 #include "aom_dsp/fwd_txfm.h"
+#include "aom_dsp/inv_txfm.h"
 
 #include "utils/luma2png.h"
 
+#define LUMA_PLANE (0)
+
 int main(int _argc,char **_argv) {
+  int x,y;
+  int bx,by; // Position inside the block
+  int fx,fy; // Position inside frame
+
   // Open Y4M
   FILE *fin = fopen(_argv[1], "rb");
   video_input vid;
@@ -20,108 +27,111 @@ int main(int _argc,char **_argv) {
   video_input_get_info(&vid,&info);
   video_input_ycbcr f;
   int ret1=video_input_fetch_frame(&vid,f,NULL);
+
   const int height = info.pic_h;
   const int width = info.pic_w;
+  const int image_square = height * width;
+  // Out image is subsampled by 2
+  const int out_width = width >> 1;
+  const int out_height = height >> 1;
+  const int out_image_square = out_height * out_width;
+
   const int block_size = 8;
   const int block_square = block_size * block_size;
-  const int image_square = height * width;
+  const int big_block_size = block_size << 1;
+  const int big_block_square = big_block_size * big_block_size;
+  const int bottom_left = block_size * big_block_size;
+  const int bottom_right = bottom_left + block_size;
 
-  tran_low_t block[64] = {0}; // original block copied in from Y4M
-  tran_low_t dct_block[64] = {0}; // DCT transformed block
-  tran_low_t tf_block[256] = {0}; // TF block
-  uint8_t idct_block[256] = {0}; // inverse DCT transform block
+  // Original block copied from Y4M
+  tran_low_t *block = (tran_low_t*) calloc(big_block_square,
+		  sizeof(tran_low_t));
+  // Holds 1 transformed blocks
+  tran_low_t *dct_block = (tran_low_t*) calloc(block_square,
+		  sizeof(tran_low_t));
+  // Holds 4 transformed blocks
+  tran_low_t *dct_big_block = (tran_low_t*) calloc(big_block_square,
+		  sizeof(tran_low_t));
+  // TF block
+  tran_low_t *tf_block = (tran_low_t*) calloc(block_square,
+		  sizeof(tran_low_t));
+  // Inverse transformed block
+  uint8_t *idct_block = (uint8_t*) calloc(block_square,
+		  sizeof(tran_low_t));
+  // Final pixel block
+  uint8_t *out = (uint8_t*) calloc(out_image_square,
+		  sizeof(uint8_t));
 
-  // DCT transformed image
-  tran_low_t *dct_image = (tran_low_t*) malloc(sizeof(tran_low_t) * image_square);
-  // inverse DCT transformed image
-  uint8_t *idct_image = (uint8_t*) malloc(sizeof(uint8_t) * image_square);
-
-  int x,y;
-  int bx,by; // index inside block
-  int fx,fy; // index inside frame
-
-  for (y = 0; y < height; y += block_size) {
-    for (x = 0; x < width; x +=  block_size) {
+  for (y = 0; y < height; y += big_block_size) {
+    for (x = 0; x < width; x +=  big_block_size) {
 
       // Copy 8 bit pixels into 16 bit block coeff
-      for (by = 0; by < block_size; by++) {
+      for (by = 0; by < big_block_size; by++) {
 	fy = by + y;
-        for (bx = 0; bx < block_size; bx++) {
+        for (bx = 0; bx < big_block_size; bx++) {
 	  fx = bx + x;
-          block[by * block_size + bx] = f[0].data[fy * width + fx];
+          block[by * big_block_size + bx] = f[LUMA_PLANE].data[fy * width + fx];
         }
       }
 
-      // 8x8 DCT Transform
-      aom_fdct8x8_c(block, dct_block, block_size);
-
-      // Copy DCT block into image
+      // DCT Transform the 16 bit block coeffs
+      // Top left
+      aom_fdct8x8_c(block, dct_block, big_block_size);
       for (by = 0; by < block_size; by++) {
-	fy = by + y;
         for (bx = 0; bx < block_size; bx++) {
-	  fx = bx + x;
-          dct_image[fy * width + fx] = dct_block[by * block_size + bx];
-        }
-      }
-    }
-  }
-
-  for (y = 0; y < height; y += 16) {
-    int row = y * width;
-    for (x = 0; x < width; x += 16) {
-
-      // TF merge 4 8x8 into a 16x16
-      od_tf_up_hv_lp(tf_block, 16, &dct_image[row + x], width,
-		      block_size, block_size, block_size);
-
-      for (by = 0; by < 16; by++) {
-        for (bx = 0; bx < 16; bx++) {
-	  tf_block[by * 16 + bx] *= 2;
+          dct_big_block[by * big_block_size + bx]
+		  = dct_block[by * block_size + bx];
 	}
       }
 
-      memset(idct_block, 0, sizeof(uint8_t)*16*16);
-      aom_idct16x16_256_add_c(tf_block, idct_block, 16);
+      // Top right
+      aom_fdct8x8_c(&block[block_size], dct_block, big_block_size);
+      for (by = 0; by < block_size; by++) {
+        for (bx = 0; bx < block_size; bx++) {
+          dct_big_block[by * big_block_size + bx + block_size]
+		  = dct_block[by * block_size + bx];
+	}
+      }
+      // Bottom left
+      aom_fdct8x8_c(&block[bottom_left], dct_block, big_block_size);
+      for (by = 0; by < block_size; by++) {
+        for (bx = 0; bx < block_size; bx++) {
+          dct_big_block[bottom_left + by * big_block_size + bx]
+		  = dct_block[by * block_size + bx];
+	}
+      }
+      // Bottom right
+      aom_fdct8x8_c(&block[bottom_right], dct_block, big_block_size);
+      for (by = 0; by < block_size; by++) {
+        for (bx = 0; bx < block_size; bx++) {
+          dct_big_block[bottom_right + by * big_block_size + bx]
+		  = dct_block[by * block_size + bx];
+	}
+      }
+      // TF merge 4 8x8 into a 16x16
+      od_tf_up_hv_lp(tf_block, block_size, dct_big_block, big_block_size,
+		      block_size, block_size, block_size);
 
-      for (by = 0; by < 16; by++) {
-	fy = by + y;
-        for (bx = 0; bx < 16; bx++) {
-	  fx = bx + x;
-          idct_image[fy * width + fx] = idct_block[by * 16 + bx];
+      memset(idct_block, 0, sizeof(uint8_t) * block_square);
+      aom_idct8x8_64_add_c(tf_block, idct_block, block_size);
+
+      for (by = 0; by < block_size; by++) {
+	fy = by + (y >> 1);
+        for (bx = 0; bx < block_size; bx++) {
+	  fx = bx + (x >> 1);
+
+          // Copy the 32 bit block coeffs to 8 bit pixels
+          out[fy * out_width + fx] = idct_block[by * block_size + bx];
         }
       }
     }
   }
 
-  /*for (y = 0; y < height; y += block_size) {
-    for (x = 0; x < width; x += block_size) {
+  luma2png("aom_tf.png", out, out_width, out_height);
 
-      // Copy DCT Coeff into block
-      for (by = 0; by < block_size; by++) {
-	fy = by + y;
-        for (bx = 0; bx < block_size; bx++) {
-	  fx = bx + x;
-          dct_block[by * block_size + bx] = dct_image[fy * width + fx];
-        }
-      }
-
-      // Set idct_block to zero because inverse transform also adds.
-      memset(idct_block, 0, sizeof(uint8_t) * block_square);
-
-      //aom_idct16x16_10_add_c(dct_block, idct_block, block_size);
-      aom_idct8x8_64_add_c(dct_block, idct_block, block_size);
-
-      for (by = 0; by < block_size; by++) {
-	fy = by + y;
-        for (bx = 0; bx < block_size; bx++) {
-	  fx = bx + x;
-          idct_image[fy * width + fx] = idct_block[by * block_size + bx];
-        }
-      }
-    }
-  }*/
-  luma2png("aom_tf.png", idct_image, width, height);
-
-  free(dct_image);
-  free(idct_image);
+  free(block);
+  free(dct_block);
+  free(dct_big_block);
+  free(idct_block);
+  free(out);
 }
--- tf/daala_tf.c
+++ tf/daala_tf.c
@@ -4,7 +4,6 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 #include "dct.h"
 #include "vidinput.h"
@@ -27,66 +26,94 @@ int main(int _argc,char **_argv) {
   video_input_get_info(&vid,&info);
   video_input_ycbcr f;
   int ret1=video_input_fetch_frame(&vid,f,NULL);
+
   const int height = info.pic_h;
   const int width = info.pic_w;
-  const int block_size = 8;
-  const int big_block_size = block_size << 1;
-  const int block_square = block_size * block_size;
   const int image_square = height * width;
+  // Out image is subsampled by 2
+  const int out_width = width >> 1;
+  const int out_height = height >> 1;
+  const int out_image_square = out_height * out_width;
 
-  od_coeff block[block_square];
-  od_coeff *dct_image = (od_coeff*) malloc(sizeof(od_coeff)* image_square);
-  od_coeff *tf_image = (od_coeff*) malloc(sizeof(od_coeff)* image_square);
-  od_coeff *idct_image = (od_coeff*) malloc(sizeof(od_coeff)* image_square);
+  const int block_size = 8;
+  const int block_square = block_size * block_size;
+  const int big_block_size = block_size << 1;
+  const int big_block_square = big_block_size * big_block_size;
+  const int bottom_left = block_size * big_block_size;
+  const int bottom_right = bottom_left + block_size;
 
-  uint8_t *out = (uint8_t*) malloc(sizeof(uint8_t) * image_square);
+  // Original block copied from Y4M
+  od_coeff *block = (od_coeff*) calloc(big_block_square, sizeof(od_coeff));
+  // Holds 4 transformed blocks
+  od_coeff *dct_block = (od_coeff*) calloc(big_block_square, sizeof(od_coeff));
+  // TF block
+  od_coeff *tf_block = (od_coeff*) calloc(block_square, sizeof(od_coeff));
+  // Inverse transformed block
+  od_coeff *idct_block = (od_coeff*) calloc(block_square, sizeof(od_coeff));
+  // Final pixel block
+  uint8_t *out = (uint8_t*) calloc(out_image_square, sizeof(uint8_t));
 
-  for (y = 0; y < height; y += block_size) {
-    for (x = 0; x < width; x +=  block_size) {
+  for (y = 0; y < height; y += big_block_size) {
+    for (x = 0; x < width; x +=  big_block_size) {
 
       // Copy 8 bit pixels into a block of 32 bit coeffs
-      for (by = 0; by < block_size; by++) {
+      for (by = 0; by < big_block_size; by++) {
 	fy = by + y;
-        for (bx = 0; bx < block_size; bx++) {
+        for (bx = 0; bx < big_block_size; bx++) {
 	  fx = bx + x;
-          block[by * block_size + bx] = f[LUMA_PLANE].data[fy * width + fx];
+          block[by * big_block_size + bx] = f[LUMA_PLANE].data[fy * width + fx];
         }
       }
 
       // DCT Transform the 32 bit block coeffs
-      od_bin_fdct8x8(&dct_image[y * width + x], width, block, block_size);
-    }
-  }
+      // Top left
+      od_bin_fdct8x8(dct_block, big_block_size, block, big_block_size);
+      // Top right
+      od_bin_fdct8x8(&dct_block[block_size], big_block_size,
+		     &block[block_size], big_block_size);
+      // Bottom left
+      od_bin_fdct8x8(&dct_block[bottom_left], big_block_size,
+		     &block[bottom_left], big_block_size);
+      // Bottom right
+      od_bin_fdct8x8(&dct_block[bottom_right], big_block_size,
+		     &block[bottom_right], big_block_size);
 
-  for (y = 0; y < height; y += big_block_size) {
-    int row = y * width;
-    for (x = 0; x < width; x += big_block_size) {
-      // Perform TF on 4 transformed blocks
-      od_tf_up_hv_lp(&tf_image[row + x], width, &dct_image[row + x], width,
+      // Perform TF on the 4 transformed blocks
+      od_tf_up_hv_lp(tf_block, block_size, dct_block, big_block_size,
 		      block_size, block_size, block_size);
 
+      // Daala requires scaling the TF by half because its transforms are unit
+      // scale.
+      for (by = 0; by < block_size; by++) {
+        for (bx = 0; bx < block_size; bx++) {
+	  tf_block[by * block_size + bx] >>= 1;
+	}
+      }
+
       // Inverse transform the TF block
-      od_bin_idct16x16(&idct_image[row + x], width, &tf_image[row + x], width);
+      od_bin_idct8x8(idct_block, block_size, tf_block, block_size);
+
+      for (by = 0; by < block_size; by++) {
+	fy = by + (y >> 1);
+        for (bx = 0; bx < block_size; bx++) {
+	  fx = bx + (x >> 1);
+          // Clamp the 32 bit block coeffs to 8 bit
+          int val = idct_block[by * block_size + bx];
+          if (val < 0) val = 0;
+          if (val > 255) val = 255;
+
+          // Copy the 32 bit block coeffs to 8 bit pixels
+          out[fy * out_width + fx] = val;
+        }
+      }
     }
   }
 
-  for (y = 0; y < height; y++) {
-    int row = y * width;
-    for (x = 0; x < width; x++) {
+  luma2png("daala_tf.png", out, out_width, out_height);
 
-      // Clamp the 32 bit block coeffs to 8 bit
-      int val = idct_image[row + x];
-      if (val < 0) val = 0;
-      if (val > 255) val = 255;
-
-      // Copy the 32 bit block coeffs to 8 bit pixels
-      out[row + x] = val;
-    }
-  }
-
-  luma2png("daala_tf.png", out, width, height);
-
-  free(dct_image);
-  free(tf_image);
-  free(idct_image);
+  free(block);
+  free(dct_block);
+  free(tf_block);
+  free(idct_block);
+  free(out);
 }
