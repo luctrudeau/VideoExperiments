fa71897bb38643180a739b3cfa3eebd96f98b967
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-23 14:50:05 -0400

    CfL Experiment

---
+++ cfl/gain/experiment.m
@@ -0,0 +1,80 @@
+% tabula rasa
+clc; clear; close all
+
+% Tested with 4, 8, 16 and 32
+block_size = 8;
+bm1 = block_size -1;
+% Same N as in https://people.xiph.org/~unlord/spie_cfl.pdf
+N = block_size * block_size;
+
+im = imread('../../videos/lizard.jpg');
+[h w ~] = size(im);
+num_pix = h * w;
+yuv = rgb2ycbcr(im);
+
+% Let's worry about types later
+y_img = double(yuv(:,:,1));
+c_img = double(yuv(:,:,2));
+cfl = double(zeros(h, w, 1));
+
+for y = 1:block_size:h-bm1
+  yRange = y:y+bm1;
+  for x = 1:block_size:w-bm1
+    xRange = x:x+bm1;
+
+    by = y_img(yRange,xRange);
+    bc = c_img(yRange,xRange);
+
+    % The DC should be the sum times some scaling factor.
+    % However, we want to avoid signaling beta, so the idea
+    % is that beta=DC_PRED. DC_PRED is a prediction of the
+    % average pixel value inside the block.
+
+    % For Luma (encoder only), we will always use the
+    % average over the entire block.
+    by_avg = mean(by(:));
+
+    % For Chroma, we use DC_PRED on both the decoder side
+    % and the encoder side. For the purpose of this script,
+    % I will use the average over the entire block.  
+    beta = mean(bc(:));
+
+    % Named L and C to refer to CfL paper.
+    % However, unline the paper L and C are zero mean.
+    L = by - by_avg;
+    C = bc - beta;
+
+    % Sum of Luma == 0, because L is zero mean
+    % sL = sum(L(:));
+    assert(sum(L(:)) == 0);
+    % This is also true for sC. However, when we use
+    % beta = DC_PRED, this will no longer old.
+    sC = sum(C(:));
+    assert(sum(C(:)) == 0);
+
+    sLL = sum(L(:).^2);
+    sLC = sum(L(:) .* C(:));
+
+    % Holy simplifications Batman!
+    % Because sL == sC == 0, alpha as defined in eq.2
+    % of https://people.xiph.org/~unlord/spie_cfl.pdf
+    % a = (N * sLC - sL * sC) / (M * sLL - sC.^2)
+    % simplifies to
+    a = sLC / sLL;
+
+    % This will not be true for sC when we use beta = DC_PRED,
+    % so alpha will most likely be:
+    assert(a == sLC / (sLL - sC.^2))
+
+    cfl(yRange, xRange) = uint8(round(L * a + beta));
+  end
+end
+
+subplot(3,1,1); imshow(uint8(y_img)); title('Luma');
+subplot(3,1,2); imshow(uint8(c_img)); title('Chroma');
+subplot(3,1,3); imshow(uint8(cfl)); title('CfL');
+
+sse = sum((c_img(:) - cfl(:)).^2)
+psnr = 20 * log10(255) - 10 * log10(sse/(num_pix))
+
+imwrite(uint8(cfl), 'cfl.png');
