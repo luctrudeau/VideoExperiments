776e0dbb8d4ad48d6c697f91d32b44c2b806a627
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-24 16:29:26 -0400

    Added quantization

--- cfl/gain/cfl_dcpred_int_q.m
+++ cfl/gain/cfl_dcpred_int_q.m
@@ -7,6 +7,15 @@ bm1 = block_size - 1;
 % Same N as in https://people.xiph.org/~unlord/spie_cfl.pdf
 N = block_size * block_size;
 
+% Obtained via kmeans
+%br = [-1.0731814, -0.3952417, -0.2159633, -0.1056401, -0.0086901, 0.1379482, 0.4601128];
+%sc = [-1.629662, -0.516701, -0.273782, -0.158144, -0.0, 0.0, 0.240141, 0.680085];
+
+% Obtained with common sense (life is already complicated enough)
+% What we want is more sampling near 0.
+br = [-0.75, -0.375, -0.1875, -0.0625, 0.0625, 0.1875, 0.375, 0.75];
+sc = [-1, -0.5, -0.25, -0.125, 0, 0.125, 0.25, 0.5, 1];
+
 %im = imread('~/Videos/Hamilton.jpg');
 %im = imread('~/Videos/Meerkat.jpg');
 %im = imread('~/Videos/Owl.jpg');
@@ -52,40 +61,51 @@ for y = 1:block_size:h-bm1
     L = int16(by) - by_avg;
     C = int16(bc) - beta;
 
-    % We could process Luma to remove outliers
-    % L = L + (abs(L) > 4*std(L(:))) * (4*std(L(:)));
-
     % Sum of Luma == 0, because L is zero mean
-    % sL = sum(L(:));
+    %sL = sum(L(:));
     % This does not old anymore because of rounding
     % assert(sum(L(:)) == 0);
 
     % This is also true for sC. However, because we use
     % beta=DC_PRED, this will no longer old. However, we
     % will show that sC simplifies out.
-    % sC = sum(C(:));
+    %sC = sum(C(:));
     %assert(sum(C(:)) == 0);
 
     sLL = sum(L(:).^2);
     sLC = sum(L(:) .* C(:));
 
+    lls(k) = sLL;
+    lcs(k) = sLC;
+
     % Because sL == 0, alpha as defined in eq.2
     % of https://people.xiph.org/~unlord/spie_cfl.pdf
     % a = (N * sLC - sL * sC) / (N * sLL - sL.^2)
     % the denominator is
-    %den = (N * sLL - sL.^2);
+    % den = (N * sLL - sL.^2);
     % it simplifies to
     den = sLL;
     if den != 0
       % the numerator is
-      % a = (N * sLC - sL * sC) / den;
+      %a = (N * sLC - sL * sC) / den;
       % it simplifies to
       a = sLC / den;
     else
       a = 0;
     end
 
+    % Instead of quantifying and signaling a, it might be
+    % possible to quantify and signal sLC.
+
+    % Probably the worst way to do this
+    i = 1;
+    while i < 9 && a > br(i)
+      i++;
+    end
+    a = sc(i);
+
     as(k) = a;
+
     % Question: Should we take the rounding error into
     % consideration?
     % Answer: Ignoring the rounding error gives better
@@ -105,8 +125,8 @@ subplot(2,2,1:2); plot(as, 'x'); title('Alpha');
 subplot(2,2,3); imshow(cfl); title('CfL');
 subplot(2,2,4); imshow(cfl_err); title('Chroma - CfL');
 
-imwrite(cfl, 'dcpred_cfl_int.png');
-imwrite(cfl_err, 'dcpred_cfl_err_int.png');
+imwrite(cfl, 'dcpred_cfl_int_q.png');
+imwrite(cfl_err, 'dcpred_cfl_err_int_q.png');
 
 sse = sum((c_img(:) - cfl(:)).^2)
 psnr = 20 * log10(255) - 10 * log10(sse/(num_pix))
